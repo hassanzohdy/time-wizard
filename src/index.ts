4ba6a17c64930d5705a1f27d5b4aa1ce74026be8
Hasan Zohdy <hassanzohdy@gmail.com>
2023-04-24 17:01:35 +0200

    "Fixed dates"

--- src/index.ts
+++ src/index.ts
@@ -14,114 +14,52 @@ export function initializeDayjs() {
   dayjs.extend(customParseFormat);
 }
 
-interface TimeWizardContract {
-  date: (date?: Parameters<typeof dayjs>[0]) => Dayjs;
-  now: () => Dayjs;
-  today: () => Dayjs;
-  yesterday: () => Dayjs;
-  tomorrow: () => Dayjs;
-  toUTC: (date: Date) => Date;
-  fromUTC: (date: Date) => Date;
-  getTimezone: () => string;
-  setTimezone: (timezone: string) => void;
-}
-
-export class TimeWizard implements TimeWizardContract {
-  constructor(private timezone: string = "UTC") {}
-
-  /**
-   * Returns a new `Dayjs` instance representing the specified date and time in the current timezone.
-   * If no date is specified, returns a new `Dayjs` instance representing the current date and time in the current timezone.
-   */
-  public date(date?: Parameters<typeof dayjs>[0]): Dayjs {
-    if (date) {
-      const dateInTimezone = dayjs(date).tz(this.timezone);
-      return dayjs(dateInTimezone.format("YYYY-MM-DDTHH:mm:ss.SSSZ"));
-    }
-    return dayjs().tz(this.timezone);
-  }
-
-  /**
-   * Returns a new `Dayjs` instance representing the current date and time in the current timezone.
-   */
-  public now(): Dayjs {
-    return dayjs().tz(this.timezone);
-  }
-
-  /**
-   * Returns a new `Dayjs` instance representing the start of the current day in the current timezone.
-   */
-  public today(): Dayjs {
-    return this.date().startOf("day");
-  }
-
-  /**
-   * Returns a new `Dayjs` instance representing the start of the previous day in the current timezone.
-   */
-  public yesterday(): Dayjs {
-    return this.date().subtract(1, "day").startOf("day");
-  }
-
-  /**
-   * Returns a new `Dayjs` instance representing the start of the next day in the current timezone.
-   */
-  public tomorrow(): Dayjs {
-    return this.date().add(1, "day").startOf("day");
-  }
-
-  /**
-   * Converts a `Date` object to the equivalent date and time in the UTC timezone.
-   */
-  public toUTC(date: Date): Date {
-    return dayjs(date).utc().toDate();
-  }
-
-  /**
-   * Converts a `Date` object representing a UTC date and time to the equivalent date and time in the current timezone.
-   */
-  public fromUTC(date: Date): Date {
-    return dayjs(date).tz(this.timezone).toDate();
-  }
-
-  /**
-   * Returns the current timezone.
-   */
-  public getTimezone(): string {
-    return this.timezone;
-  }
-
-  /**
-   * Sets the current timezone to the specified timezone.
-   */
-  public setTimezone(timezone: string): void {
-    this.timezone = timezone;
-  }
-}
-
-export const now = () => {
-  return new TimeWizard(config.get("app.timezone", "UTC")).now();
+export const now = (): Date => {
+  return toUTC(new Date());
 };
 
 export const today = () => {
-  return new TimeWizard(config.get("app.timezone", "UTC")).today();
+  // get today's date in UTC timezone
+  return dayjs(now()).startOf("day").toDate();
 };
 
 export const yesterday = () => {
-  return new TimeWizard(config.get("app.timezone", "UTC")).yesterday();
+  // get yesterday's date in UTC timezone
+  return dayjs(now()).subtract(1, "day").startOf("day").toDate();
 };
 
 export const tomorrow = () => {
-  return new TimeWizard(config.get("app.timezone", "UTC")).tomorrow();
+  // get tomorrow's date in UTC timezone
+  return dayjs(now()).add(1, "day").startOf("day").toDate();
 };
+
+export const thisMonth = () => {
+  // get this month's date in UTC timezone
+  return dayjs(now()).startOf("month").toDate();
+}
+
+export const thisYear = () => {
+  // get this year's date in UTC timezone
+  return dayjs(now()).startOf("year").toDate();
+}
 
 export const toUTC = (date: Date) => {
-  return new TimeWizard(config.get("app.timezone", "UTC")).toUTC(date);
+  // convert the given date object to UTC timezone
+  return new Date(
+    date.toLocaleString("en-US", {
+      timeZone: "UTC",
+    })
+  );
 };
 
-export const fromUTC = (date: Date) => {
-  return new TimeWizard(config.get("app.timezone", "UTC")).fromUTC(date);
-};
-
-export const timeWizard = (timezone = config.get("app.timezone", "UTC")) => {
-  return new TimeWizard(timezone);
+export const fromUTC = (
+  date: Date,
+  timezone: string = config.get("app.timezone", "UTC")
+) => {
+  // convert the given UTC date object to the given timezone
+  return new Date(
+    date.toLocaleString("en-US", {
+      timeZone: timezone,
+    })
+  );
 };
